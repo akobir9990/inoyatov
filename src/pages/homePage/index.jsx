d04f10d34c135ad6aad116c67b0106edb241e453
akobir9990 <akobir.qoshshayev@gmail.com>
2024-12-02 05:24:05 +0500

    changed science name

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -3,7 +3,7 @@ import { Box, Container } from "@mui/material";
 import "./style.css";
 function index() {
   return (
-    <Container className="min-h-[calc(100vh-56px)]">
+    <Container className="min-h-[calc(100vh-76px)] mt-5">
       <Box className="relative p-2 md:p-5 flex flex-col gap-10 items-center py-10 backdrop-blur-md bg-white/30 rounded-lg min-h-[90vh]">
         <h1 className="md:text-[30px] text-[15px] text-center p-0">
           “TIQXMMI” MTU Buxoro tabiiy resurslarini boshqarish instituti <br />
@@ -14,11 +14,11 @@ function index() {
         </h2>
 
         <h1 className="md:text-[50px] text-[35px] my-5 text-center font-bold">
-          Suv ta'minoti
+          Qishloq va yaylovlar suv ta'minoti
         </h1>
         <h3 className="text-center leading-6">
           “TIQXMMI” MTU Buxoro tabiiy resurslarini boshqarish instituti <br />
-          60812900 - "Suv ta’minoti muhandislik tizimi" yo'nalishi uchun
+          60812300 - "Suv xo'jaligi va melioratsiya" yo'nalishi uchun
         </h3>
 
         <p className="absolute font-bold bottom-3">Buxoro-2024</p>
